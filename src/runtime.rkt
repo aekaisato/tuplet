cd09a8cb4ba1233c146b943701c344e408a0b39b
aekaisato <35282290+aekaisato@users.noreply.github.com>
2025-02-26 22:22:42 -0500

    improved documentation and contracts

--- src/runtime.rkt
+++ src/runtime.rkt
@@ -4,21 +4,28 @@
 
 (define/contract (oneshot? x) (-> any/c boolean?) (or (tuplet? x) (note? x)))
 
+; represents a single sound that can be played
 (define-struct/contract note ([sound rsound?] [chop? boolean?]))
 
-(define-struct/contract tuplet ([beats rational?] [contents (listof oneshot?)]))
+; represents a series of oneshots that "squeeze" to fit into the given number of beats
+(define-struct/contract tuplet ([beats (and/c (not/c negative?) rational?)] [contents (listof oneshot?)]))
 
-(define-struct/contract track ([name string?] [bpm rational?] [measures (listof tuplet?)]))
+; represents a full audio track
+(define-struct/contract track ([name string?] [bpm (and/c positive? rational?)] [measures (listof tuplet?)]))
 
+; intermediary format for notes that includes the sample to start playing and the sample to stop playing
 (define-struct/contract note-assembly ([sound rsound?] [in-sample (not/c negative?)] [out-sample (or/c false? (not/c negative?))]))
 
+; intermediate format for tracks intended to map to input for rsound's assemble
 (define-struct/contract track-assembly ([assembly (listof note-assembly?)]))
 
+; convert a number from beats-per-minute to samples-per-beat
 (define/contract (bpm-to-samples bpm)
-  (-> rational? nonnegative-integer?)
+  (-> (and/c positive? rational?) nonnegative-integer?)
   (define sample-rate (default-sample-rate))
   (round (* (* (/ 1 bpm) 60) sample-rate)))
 
+; squeezes a track by placing notes at in-out points defined by structure, an offset, and the current length of a beat
 (define/contract (squeeze-track track)
   (-> track? track-assembly?)
   (define samples-per-beat (bpm-to-samples (track-bpm track)))
@@ -31,6 +38,7 @@
   (define assembly (flatten (helper (track-measures track) 0)))
   (track-assembly assembly))
 
+; squeezes a list of oneshots by placing notes at in-out points defined by structure, an offset, and the current length of a beat
 (define/contract (squeeze-oneshot-list ls starting-sample samples-per-beat)
   (-> (listof oneshot?) (not/c negative?) (not/c negative?) (listof note-assembly?))
   (match ls
@@ -44,6 +52,7 @@
      (define tup-assembly (squeeze-tuplet (tuplet beats contents) starting-sample (/ samples-per-beat beats)))
      (append tup-assembly (squeeze-oneshot-list rest next-beat samples-per-beat))]))
 
+; squeezes a tuplet by placing notes at in-out points defined by structure, an offset, and the current length of a beat
 (define/contract (squeeze-tuplet tup starting-sample samples-per-beat)
   (-> tuplet? (not/c negative?) (not/c negative?) (listof note-assembly?))
   (define tup-beats (tuplet-beats tup))
@@ -51,6 +60,7 @@
   (define ratio (/ tup-beats (length tup-contents)))
   (squeeze-oneshot-list tup-contents starting-sample (* samples-per-beat ratio)))
 
+; assembles a track-assembly to a single rsound that can be played or exported as audio
 (define/contract (assemble-track assembly)
   (-> track-assembly? rsound?)
   (define asm (track-assembly-assembly assembly))
@@ -64,23 +74,25 @@
                         (list rs in)) asm))
   (assemble rs-asm))
 
-  (define k (note (rs-read (normalize-path "../examples/samples/k.wav")) #t))
-  (define s (note (rs-read (normalize-path "../examples/samples/s.wav")) #t))
-  (define r (note (rs-read (normalize-path "../examples/samples/r.wav")) #t))
-  (define h (note (rs-read (normalize-path "../examples/samples/h.wav")) #f))
+; scratch area
 
-  (define tracktest
-    (track "test1" 180
-           (list
-            (tuplet 4 (list k r s r))
-            (tuplet 4 (list k s (tuplet 1 (list k k)) h))
-            (tuplet 4 (list k r s r))
-            (tuplet 4 (list s s s s s s s s))
-            (tuplet 4 (list (tuplet 1 (list k k)) (tuplet 1 (list s (tuplet 1 (list s k)))) (tuplet 1 (list r r k k)) (tuplet 1 (list s (tuplet 1 (list s h))))))
-            )))
+(define k (note (rs-read (normalize-path "../examples/samples/k.wav")) #t))
+(define s (note (rs-read (normalize-path "../examples/samples/s.wav")) #t))
+(define r (note (rs-read (normalize-path "../examples/samples/r.wav")) #t))
+(define h (note (rs-read (normalize-path "../examples/samples/h.wav")) #f))
 
-  (define tracktest-asm (squeeze-track tracktest))
+(define tracktest
+  (track "test1" 180
+         (list
+          (tuplet 4 (list k r s r))
+          (tuplet 4 (list k s (tuplet 1 (list k k)) h))
+          (tuplet 4 (list k r s r))
+          (tuplet 4 (list s s s s s s s s))
+          (tuplet 4 (list (tuplet 1 (list k k)) (tuplet 1 (list s (tuplet 1 (list s k)))) (tuplet 1 (list r r k k)) (tuplet 1 (list s (tuplet 1 (list s h))))))
+          )))
 
-  (define assembled (assemble-track tracktest-asm))
+(define tracktest-asm (squeeze-track tracktest))
 
-  (play assembled)
+(define assembled (assemble-track tracktest-asm))
+
+(play assembled)
