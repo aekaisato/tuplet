84edbc388e32358b693059bcd50912659b64bda2
aekaisato <35282290+aekaisato@users.noreply.github.com>
2025-02-26 22:12:21 -0500

    fixed chop? option

--- src/runtime.rkt
+++ src/runtime.rkt
@@ -10,7 +10,7 @@
 
 (define-struct/contract track ([name string?] [bpm rational?] [measures (listof tuplet?)]))
 
-(define-struct/contract note-assembly ([sound rsound?] [in-sample (not/c negative?)] [out-sample (or (not/c negative?) false?)]))
+(define-struct/contract note-assembly ([sound rsound?] [in-sample (not/c negative?)] [out-sample (or/c false? (not/c negative?))]))
 
 (define-struct/contract track-assembly ([assembly (listof note-assembly?)]))
 
@@ -55,30 +55,32 @@
   (-> track-assembly? rsound?)
   (define asm (track-assembly-assembly assembly))
   (define rs-asm (map (lambda (note-asm)
-         (define s (note-assembly-sound note-asm))
-         (define in (round (note-assembly-in-sample note-asm)))
-         (define out (round (note-assembly-out-sample note-asm)))
-         (define rs (if out (clip s 0 (min (- out in) (rs-frames s))) s))
-         (list rs in)) asm))
+                        (define s (note-assembly-sound note-asm))
+                        (define s-len (rs-frames s))
+                        (define in (round (note-assembly-in-sample note-asm)))
+                        (define out-maybe (note-assembly-out-sample note-asm))
+                        (define out (if out-maybe (round out-maybe) (+ in s-len)))
+                        (define rs (if out (clip s 0 (min (- out in) s-len)) s))
+                        (list rs in)) asm))
   (assemble rs-asm))
 
-(define k (note (rs-read (normalize-path "../examples/samples/k.wav")) #t))
-(define s (note (rs-read (normalize-path "../examples/samples/s.wav")) #t))
-(define r (note (rs-read (normalize-path "../examples/samples/r.wav")) #t))
-(define h (note (rs-read (normalize-path "../examples/samples/h.wav")) #t))
+  (define k (note (rs-read (normalize-path "../examples/samples/k.wav")) #t))
+  (define s (note (rs-read (normalize-path "../examples/samples/s.wav")) #t))
+  (define r (note (rs-read (normalize-path "../examples/samples/r.wav")) #t))
+  (define h (note (rs-read (normalize-path "../examples/samples/h.wav")) #f))
 
-(define tracktest
-  (track "test1" 180
-         (list
-          (tuplet 4 (list k r s r))
-          (tuplet 4 (list k s (tuplet 1 (list k k)) h))
-          (tuplet 4 (list k r s r))
-          (tuplet 4 (list s s s s s s s s))
-          (tuplet 4 (list (tuplet 1 (list k k)) (tuplet 1 (list s (tuplet 1 (list s k)))) (tuplet 1 (list r r k k)) (tuplet 1 (list s (tuplet 1 (list s h))))))
-          )))
+  (define tracktest
+    (track "test1" 180
+           (list
+            (tuplet 4 (list k r s r))
+            (tuplet 4 (list k s (tuplet 1 (list k k)) h))
+            (tuplet 4 (list k r s r))
+            (tuplet 4 (list s s s s s s s s))
+            (tuplet 4 (list (tuplet 1 (list k k)) (tuplet 1 (list s (tuplet 1 (list s k)))) (tuplet 1 (list r r k k)) (tuplet 1 (list s (tuplet 1 (list s h))))))
+            )))
 
-(define tracktest-asm (squeeze-track tracktest))
+  (define tracktest-asm (squeeze-track tracktest))
 
-(define assembled (assemble-track tracktest-asm))
+  (define assembled (assemble-track tracktest-asm))
 
-(play assembled)
+  (play assembled)
