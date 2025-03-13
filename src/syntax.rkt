e226ce6cfd680561636cb9039c920ad6a995d355
Aiden Sato <aiden@aekai.dev>
2025-03-13 17:29:10 -0400

    improve handling of prim ops for binding

--- src/syntax.rkt
+++ src/syntax.rkt
@@ -1,32 +1,36 @@
 #lang racket
 
-(require (prefix-in rt: "runtime.rkt") (prefix-in rsound: rsound) syntax-spec-v3 (for-syntax syntax/parse))
+(require (prefix-in rt: "runtime.rkt") (prefix-in rsound: rsound) syntax-spec-v3 (for-syntax syntax/parse racket/syntax))
 
 (provide track (rename-out [tlet let] [rt:load load] [rt:save! save!] [rt:play! play!]))
 
 (begin-for-syntax
   ; a oneshot is either a note, tuplet, or pattern
   (define-syntax-class oneshot
-    (pattern (~or t:tuplet n:note p:pattern)))
+    (pattern (~or t:tup n:note p:pat)))
 
   ; a note within a track should just be a binding to a note
   (define-syntax-class note
     (pattern n:id))
 
   ; a tuplet is a number of beats, followed by zero or more oneshots
-  (define-syntax-class tuplet
+  (define-syntax-class tup
     (pattern (beats:number oneshot:oneshot ...)))
 
   ; a pattern is a series of zero or more oneshots
-  (define-syntax-class pattern
+  (define-syntax-class pat
     (pattern (oneshot:oneshot ...)))
+
+  ; a prim is a built-in function that can appear on the right side of a tlet bind
+  (define-syntax-class prim
+    (pattern (~or (~datum load))))
   )
 
 (syntax-spec
  ; track - binds a series of tuplets as a squeezed track
  ; <track> := (track [Datum] [Rational] (<tuplet>...+))
  (host-interface/definitions
-  (track name:racket-var bpm:racket-expr measure:tuplet ...+)
+  (track name:racket-var bpm:racket-expr measure:tup ...+)
   #:binding (export name)
   #`(define name (rt:squeeze-track (rt:track #,(symbol->string (syntax->datum #'name)) bpm (list (compile-tuplet measure) ...)))))
 
@@ -44,31 +48,32 @@
 ; there are some complications involving handling #%host-expression
 (define-syntax compile-tuplet
   (syntax-parser
-    [(_ (beats:number oneshot ...)) #'(rt:tuplet beats (list (compile-oneshot oneshot) ...))]))
+    [(_ t:tup) #'(rt:tuplet t.beats (list (compile-oneshot t.oneshot) ...))]))
 
 ; Syntax -> Syntax
 ; converts a pattern syntax object to a runtime pattern struct
 ; there are some complications involving handling #%host-expression
 (define-syntax compile-pattern
   (syntax-parser
-    [(_ (oneshot ...)) #'(rt:pattern (list (compile-oneshot oneshot) ...))]))
+    [(_ p:pat) #'(rt:pattern (list (compile-oneshot p.oneshot) ...))]))
 
 ; Syntax -> Syntax
 ; converts a oneshot syntax object to its respective runtime struct, depending on its shape
 ; there are some complications involving handling #%host-expression
 (define-syntax compile-oneshot
   (syntax-parser
-    [(_ note:id) #'note]
-    [(_ (beats:number oneshot ...)) #'(compile-tuplet (beats oneshot ...))]
-    [(_ (oneshot ...)) #'(compile-pattern (oneshot ...))]))
+    [(_ n:note) #'n]
+    [(_ t:tup) #'(compile-tuplet t)]
+    [(_ p:pat) #'(compile-pattern p)]))
 
 ; Syntax -> Syntax
 ; if the bind is a known expression, returns that expression
 ; otherwise, compiles oneshot depending on shape
 (define-syntax compile-bind
   (syntax-parser
-    #:datum-literals (#%host-expression load)
-    [(_ (#%host-expression (load arg ...))) #'(rt:load arg ...)]
-    [(_ (#%host-expression note:id)) #'note]
-    [(_ (#%host-expression (beats:number oneshot ...))) #'(compile-tuplet (beats oneshot ...))]
-    [(_ (#%host-expression (oneshot ...))) #'(compile-pattern (oneshot ...))]))
+    [(_ (#%host-expression (op:prim arg ...)))
+     (define/with-syntax fmt-op (format-symbol "rt:~a" #'op))
+     #'(fmt-op arg ...)]
+    [(_ (#%host-expression n:note)) #'n]
+    [(_ (#%host-expression t:tup)) #'(compile-tuplet t)]
+    [(_ (#%host-expression p:pat)) #'(compile-pattern p)]))
