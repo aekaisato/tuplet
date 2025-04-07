0e08c11c9de2cfe24cb267033b3a9cd01a97cf00
aekaisato <35282290+aekaisato@users.noreply.github.com>
2025-04-06 21:03:17 -0400

    updated complex example

--- examples/complex-example.rkt
+++ examples/complex-example.rkt
@@ -7,13 +7,20 @@
 (let three-two (: (k1 k1) (s1 s1 s1)))
 (let s-pitch2 (pitch s1 2))
 (let s-pitch4 (pitch s1 4))
+(let k-down (pitch k2 -2))
+(let s-rev (reverse s1))
 (let h-nochop (set-chop h5 #f))
 
 (track complex_example 180
-       (4 k1 r1 s1 r1)
-       (4 three-two three-two)
-       (4 k1 (r1 (2 s1 _ r1 _)))
-       (4 s1 s1 s1 s1 s3 s3 (1 s-pitch2 s-pitch2) (1 s-pitch4 s-pitch4))
-       (4 (1 k1 k1) (1 s1 (1 s1 k1)) (1 r1 r1 k1 k1) (1 s1 (1 s1 h-nochop))))
+       (4 (1 k1 k1) (1 s1 (1 s1 k1)) (1 r1 r2 k2 k1) (1 s1 (1 s3 s1)))
+       (4 (1 k1 k1) (1 s1 (1 k1 k1)) (2 s1 k2 k2 s1 k2 k2 (2 h5)))
+       (4 (1 k1 k1 k1) (1 s1 h5) (1 k2 k2 k2) (1 s1 (1 s-pitch2 s-pitch4)))
+       (4 (1 k1 k1) (1 s1 (1 s1 k1)) (1 k1 k1 k2 k2) (1 s1 s1 s1 s1 s1 s1 s1))
+
+       (4 (1 s-pitch2 (1 k1 k1)) (1 s3 s3 s3) (1 h5 (1 k1 k1)) (1 s1 s2 s1))
+       (4 (1 (s1 s3 s1 s3 s1 s3 s1 s3)) (1 h5 h5) (1 k-down (1 k-down k-down)) (1 s1 s-rev))
+       (4 (1 s-pitch2 (1 k1 k1)) (1 s1 s1) (1 (1 k1 k1) s1) (1 s1 (1 s-pitch2 s-pitch2 s-pitch4)))
+       (4 (1 k1 s-rev) (1 k1 s-rev) (1 (: (k1 k1) (s1 s1 s1))) (1 k1 (1 s-pitch2 s-pitch2 s-pitch2 s-pitch2 s-pitch2)))
+       )
 
 (play! complex_example)
