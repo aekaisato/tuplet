c090270b811f9d9e30399d059b0bdf92d637f09b
aekaisato <35282290+aekaisato@users.noreply.github.com>
2025-04-06 21:12:03 -0400

    remove unnecessary lines from complex example

--- examples/complex-example.rkt
+++ examples/complex-example.rkt
@@ -4,12 +4,10 @@
 
 (let (k1 k2 s1 (3 h1 s2 h2 s3 k3 k4) s4 (1 r1 r2) k5 k6 s5 (3 h3 s6 h4 s7 (2 s8)) (2 h5)) (load "./samples/cw_amen02_165.wav"))
 (let _ (1))
-(let three-two (: (k1 k1) (s1 s1 s1)))
 (let s-pitch2 (pitch s1 2))
 (let s-pitch4 (pitch s1 4))
 (let k-down (pitch k2 -2))
 (let s-rev (reverse s1))
-(let h-nochop (set-chop h5 #f))
 
 (track complex_example 180
        (4 (1 k1 k1) (1 s1 (1 s1 k1)) (1 r1 r2 k2 k1) (1 s1 (1 s3 s1)))
