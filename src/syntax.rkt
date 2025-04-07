dfb925b65faa1c87b52f20d29f8458fecf49f26d
aekaisato <35282290+aekaisato@users.noreply.github.com>
2025-04-06 23:59:10 -0400

    add tests for new syntax forms

--- src/syntax.rkt
+++ src/syntax.rkt
@@ -358,6 +358,10 @@
   (tlet _ (1))
   (tlet three-two (: (k k) (s s s)))
   (tlet nested ((1 k k) (1 s (1 s k))))
+
+  ; pattern tlet examples
+  (tlet (pk ps pr ph) (load (test-file-path "ksrh.wav")))
+  (tlet (pk1 pk2 ps1 (3 ph1 ps2 ph2 ps3 pk3 pk4) ps4 (1 pr1 pr2) pk5 pk6 ps5 (3 ph3 ps6 ph4 ps7 (2 ps8)) (2 ph5)) (load (sample-path "cw_amen02_165.wav")))
   
   ; track examples
   (track out_syntax_integration_1 122.5 (4 k s k s) (4 k s (1 k k) triplet))
@@ -367,22 +371,56 @@
        (4 k (r (2 s _ r _)))
        (4 s s s s s s s s)
        (4 (1 k k) (1 s (1 s k)) (1 r r k k) (1 s (1 s h))))
+  (track out_syntax_integration_3 180
+         (4 (1 pk1 pk1) (1 ps1 (1 ps1 pk1)) (1 pr1 pr2 pk2 pk1) (1 ps1 (1 ps3 ps1)))
+         (4 (1 pk1 pk1) (1 ps1 (1 pk1 pk1)) (2 ps1 pk2 pk2 ps1 pk2 pk2 (2 ph5)))
+         (4 (1 pk1 pk1 pk1) (1 ps1 ph5) (1 pk2 pk2 pk2) (1 ps1 (1 ps1 ps2)))
+         (4 (1 pk1 pk1) (1 ps1 (1 ps1 pk1)) (1 pk1 pk1 pk2 pk2) (1 ps1 ps1 ps1 ps1 ps1 ps1 ps1))
+         (4 (1 ps1 (1 pk1 pk1)) (1 ps3 ps3 ps3) (1 ph5 (1 pk1 pk1)) (1 ps1 ps2 ps1))
+         (4 (1 (ps1 ps3 ps1 ps3 ps1 ps3 ps1 ps3)) (1 ph5 ph5) (1 pk2 (1 pk2 pk2)) (1 ps1 ps3))
+         (4 (1 ps1 (1 pk1 pk1)) (1 ps1 ps1) (1 (1 pk1 pk1) ps1) (1 ps1 (1 ps1 ps1 ps2)))
+         (4 (1 pk1 ps3) (1 pk1 ps3) (1 (: (pk1 pk1) (ps1 ps1 ps1))) (1 pk1 (1 ps1 ps1 ps1 ps1 ps1))))
 
   ; track output examples
   (define ex-track-integration-1-out (rt:track "out_exp_syntax_integration_1" 122.5 (list (rt:tuplet 4 (list k s k s)) (rt:tuplet 4 (list k s (rt:tuplet 1 (list k k)) triplet)))))
-  (define ex-track-integration-2-out (rt:track "out_exp_syntax_integration_2" 180
-                                            (list
-                                             (rt:tuplet 4 (list k r s r))
-                                             (rt:tuplet 4 (list three-two three-two))
-                                             (rt:tuplet 4 (list k (rt:pattern (list r (rt:tuplet 2 (list s _ r _))))))
-                                             (rt:tuplet 4 (list s s s s s s s s))
-                                             (rt:tuplet 4 (list (rt:tuplet 1 (list k k)) (rt:tuplet 1 (list s (rt:tuplet 1 (list s k)))) (rt:tuplet 1 (list r r k k)) (rt:tuplet 1 (list s (rt:tuplet 1 (list s h)))))))))
+  (define ex-track-integration-2-out
+    (rt:track
+     "out_exp_syntax_integration_2" 180
+     (list
+      (rt:tuplet 4 (list k r s r))
+      (rt:tuplet 4 (list three-two three-two))
+      (rt:tuplet 4 (list k (rt:pattern (list r (rt:tuplet 2 (list s _ r _))))))
+      (rt:tuplet 4 (list s s s s s s s s))
+      (rt:tuplet 4 (list (rt:tuplet 1 (list k k)) (rt:tuplet 1 (list s (rt:tuplet 1 (list s k)))) (rt:tuplet 1 (list r r k k)) (rt:tuplet 1 (list s (rt:tuplet 1 (list s h)))))))))
+  (define ex-track-integration-3-out
+    (rt:track
+     "out_exp_syntax_integration_3" 180
+     (list
+      (rt:tuplet 4 (list (rt:tuplet 1 (list pk1 pk1)) (rt:tuplet 1 (list ps1 (rt:tuplet 1 (list ps1 pk1)))) (rt:tuplet 1 (list pr1 pr2 pk2 pk1)) (rt:tuplet 1 (list ps1 (rt:tuplet 1 (list ps3 ps1))))))
+      (rt:tuplet 4 (list (rt:tuplet 1 (list pk1 pk1)) (rt:tuplet 1 (list ps1 (rt:tuplet 1 (list pk1 pk1)))) (rt:tuplet 2 (list ps1 pk2 pk2 ps1 pk2 pk2 (rt:tuplet 2 (list ph5))))))
+      (rt:tuplet 4 (list (rt:tuplet 1 (list pk1 pk1 pk1)) (rt:tuplet 1 (list ps1 ph5)) (rt:tuplet 1 (list pk2 pk2 pk2)) (rt:tuplet 1 (list ps1 (rt:tuplet 1 (list ps1 ps2))))))
+      (rt:tuplet 4 (list (rt:tuplet 1 (list pk1 pk1)) (rt:tuplet 1 (list ps1 (rt:tuplet 1 (list ps1 pk1)))) (rt:tuplet 1 (list pk1 pk1 pk2 pk2)) (rt:tuplet 1 (list ps1 ps1 ps1 ps1 ps1 ps1 ps1))))
+      (rt:tuplet 4 (list (rt:tuplet 1 (list ps1 (rt:tuplet 1 (list pk1 pk1)))) (rt:tuplet 1 (list ps3 ps3 ps3)) (rt:tuplet 1 (list ph5 (rt:tuplet 1 (list pk1 pk1)))) (rt:tuplet 1 (list ps1 ps2 ps1))))
+      (rt:tuplet 4 (list (rt:tuplet 1 (list ps1 ps3 ps1 ps3 ps1 ps3 ps1 ps3)) (rt:tuplet 1 (list ph5 ph5)) (rt:tuplet 1 (list pk2 (rt:tuplet 1 (list pk2 pk2)))) (rt:tuplet 1 (list ps1 ps3))))
+      (rt:tuplet 4 (list (rt:tuplet 1 (list ps1 (rt:tuplet 1 (list pk1 pk1)))) (rt:tuplet 1 (list ps1 ps1)) (rt:tuplet 1 (list (rt:tuplet 1 (list pk1 pk1)) ps1)) (rt:tuplet 1 (list ps1 (rt:tuplet 1 (list ps1 ps1 ps2))))))
+      (rt:tuplet 4 (list (rt:tuplet 1 (list pk1 ps3)) (rt:tuplet 1 (list pk1 ps3)) (rt:polyrhythm (list (rt:pattern (list pk1 pk1)) (rt:pattern (list ps1 ps1 ps1)))) (rt:tuplet 1 (list pk1 (rt:tuplet 1 (list ps1 ps1 ps1 ps1 ps1)))))))))
   
-  ; tlet syntax spec tests
+  
+  ; tlet tests
   (check-syntax-except (tlet 88 (load ".")) "expected identifier")
   (check-syntax-except (tlet name #'"not a tuplet expr") "not an expression")
+  (check-syntax-except (tlet (7 k1 s1 k2 s2) (load ".")) "expected pat")
+
+  ; pattern tlet tests
+  (check-equal? (sq:note-sound pk) (sq:note-sound (load (test-file-path "patternbind_k.wav"))))
+  (check-equal? (sq:note-sound ps) (sq:note-sound (load (test-file-path "patternbind_s.wav"))))
+  (check-equal? (sq:note-sound pr) (sq:note-sound (load (test-file-path "patternbind_r.wav"))))
+  (check-equal? (sq:note-sound ph) (sq:note-sound (load (test-file-path "patternbind_h.wav"))))
+  ; clipping a sound sets in/out points, instead of creating a new vector; rs-equal? handles this
+  (check-true (rsound:rs-equal? (sq:note-sound pk1) (sq:note-sound (load (test-file-path "amen_pat_k1.wav")))))
+  (check-true (rsound:rs-equal? (sq:note-sound ps5) (sq:note-sound (load (test-file-path "amen_pat_s5.wav")))))
   
-  ; track syntax spec tests
+  ; track tests
   (check-syntax-except (track 99 99 (4 k s k s)) "expected identifier")
   (check-syntax-except (track invalid_bpm "sdf" (4 k s k s)) "expected number")
   (check-syntax-except (track invalid_bpm -5 (4 k s k s)) "positive rational number")
@@ -396,4 +434,7 @@
   (rt:save! out_syntax_integration_2 (test-file-path))
   (rt:save! (rt:squeeze-track ex-track-integration-2-out) (test-file-path))
   (check-equal? (rs-read (test-file-path "out_syntax_integration_2.wav")) (rs-read (test-file-path "out_exp_syntax_integration_2.wav")))
+  (rt:save! out_syntax_integration_3 (test-file-path))
+  (rt:save! (rt:squeeze-track ex-track-integration-3-out) (test-file-path))
+  (check-equal? (rs-read (test-file-path "out_syntax_integration_3.wav")) (rs-read (test-file-path "out_exp_syntax_integration_3.wav")))
   )
